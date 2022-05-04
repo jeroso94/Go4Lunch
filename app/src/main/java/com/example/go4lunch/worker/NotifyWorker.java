457cd673f97001d3f938a8d7901ddf2ede7baa5b
jeroso94 <jeroso94@gmail.com>
2022-05-04 19:15:06 +0200

    Feature-Notify_alpha

---
+++ app/src/main/java/com/example/go4lunch/worker/NotifyWorker.java
@@ -0,0 +1,93 @@
+package com.example.go4lunch.worker;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.go4lunch.R;
+import com.example.go4lunch.ui.MainActivity;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by JeroSo94 on 04/05/2022.
+ */
+public class NotifyWorker extends Worker {
+
+    public NotifyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+
+        //we set a tag to be able to cancel all work of this type if needed
+        final String WORK_TAG = "notificationWork";
+
+        /*OneTimeWorkRequest notificationWork = new OneTimeWorkRequest.Builder(NotifyWorker.class)
+                .setInitialDelay(calculateDelay(event.getDate()), TimeUnit.MILLISECONDS)
+                .addTag(WORK_TAG)
+                .build();
+
+        WorkManager.getInstance(context)
+                .enqueue(notificationWork);
+        // we can use this form to determine what happens to the existing stack
+        WorkManager.getInstance(context)
+                .beginUniqueWork(WORK_TAG, ExistingWorkPolicy.REPLACE, notificationWork);*/
+
+        PeriodicWorkRequest notificationWork =
+                new PeriodicWorkRequest.Builder(NotifyWorker.class, 15, TimeUnit.MINUTES)
+                .addTag(WORK_TAG)
+                .build();
+
+        WorkManager.getInstance(context).cancelAllWorkByTag(WORK_TAG);
+        WorkManager.getInstance(context)
+                .enqueue(notificationWork);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        remindMeBookedLunchPlace();
+        return Result.success();
+    }
+
+    private void remindMeBookedLunchPlace() {
+        final String CHANNEL_ID = "fcm_default_channel";
+        int NOTIFICATION_ID = 112233;
+        String NOTIFICATION_TAG = "GO4LUNCH";
+
+        // Create an Intent that will be shown when user will click on the Notification
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+        // Support Version >= Android 8
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Go4Lunch Messages";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        // Show notification
+        notificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, notificationBuilder.build());
+    }
+}
